e81c57dd5aaae32ae72f492e2e02ca7e95b9a22a
Arturas <arturas.danilevicius@gmail.com>
2018-10-11 11:59:26 +0300

    bandau komitinti per storma

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
+        <script type="text/javascript" src="css/java.js"></script>
             <!-- mano css -->
         <link rel="stylesheet" href="css/pagrindinis.css">
     </head>
@@ -50,12 +50,13 @@
             </div>
         </div>
         <div class="slideshow">
+            <button class="left-buttom" onclick="plusDivs(-1)">&#10094;</button>
+
             <div class="w3-content w3-display-container">
                 <a class="mySlides pirmas-slide"></a>
                 <a class="mySlides antras-slide"></a>
                 <a class="mySlides trecias-slide"></a>
                 <a class="mySlides ketvirtas-slide"></a>
-                <button class="left-buttom" onclick="plusDivs(-1)">&#10094;</button>
                 <button class="right-buttom" onclick="plusDivs(1)">&#10095;</button>
                 <div class="banner-line">
                     <div class="container">
@@ -70,10 +71,4 @@
             <div class="slide-buttoms text-center"></div>
         </div>
     </div>
-
-
-
-
-
-        
     
