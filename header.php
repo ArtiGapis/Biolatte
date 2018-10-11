7c5ff560329838bac2d3ce3f355880ca3d2746db
Arturas <arturas.danilevicius@gmail.com>
2018-10-11 11:23:33 +0300

    JS atskirimas, header ir css tvarkymas, banerio prideimas

--- header.php
+++ header.php
@@ -14,75 +14,65 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    
+
             <!-- mano css -->
         <link rel="stylesheet" href="css/pagrindinis.css">
     </head>
 <body>
     <div class="header">
-        <div class="slideshow">
-            <div class="w3-content w3-display-container">
-                <div class="ghost">
-                    <div class="cartheader img-responsive">
-                        <ul>
-                            <li><a href="#">Login</a></li>
-                            <li><a href="#">Register</a></li>
-                            <li><a href="#">Facebook</a></li>
-                            <li><a href="#"><img src="image/cart.png"></a></li>
-                        </ul>
-                    </div>
-                    <div class="menu-wraper">
-                        <div class="container">
-                            <div class="row skirimas">
-                                <div class="col-2">
-                                    <div class="logo"><img src="image/logo.png"></div>
-                                </div>
-                                <div class="col-10">
-                                    <ul>
-                                        <li><a href="#">Home</a></li>
-                                        <li><a href="#">Blog</a></li>
-                                        <li><a href="#">Events</a></li>
-                                        <li><a href="#">Store Locator</a></li>
-                                        <li><a href="#">Contact us</a></li>
-                                        <li><a href="#">How to Purchase</a></li>
-                                    </ul>
-                                </div>
-                            </div>
+        <div class="ghost">
+            <div class="cartheader img-responsive">
+                <ul>
+                    <li><a href="#">Login</a></li>
+                    <li><a href="#">Register</a></li>
+                    <li><a href="#">Facebook</a></li>
+                    <li><a href="#"><img src="image/cart.png"></a></li>
+                </ul>
+            </div>
+            <div class="menu-wraper">
+                <div class="container">
+                    <div class="row skirimas">
+                        <div class="col-2">
+                            <div class="logo"><img src="image/logo.png"></div>
+                        </div>
+                        <div class="col-10">
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Blog</a></li>
+                                <li><a href="#">Events</a></li>
+                                <li><a href="#">Store Locator</a></li>
+                                <li><a href="#">Contact us</a></li>
+                                <li><a href="#">How to Purchase</a></li>
+                            </ul>
                         </div>
                     </div>
                 </div>
-                <a class="mySlides pirmas-slide">pirmas lapas</a>
-                <a class="mySlides antras-slide">antras lapas</a>
-                <a class="mySlides trecias-slide">trecias lapas</a>
-                <a class="mySlides ketvirtas-slide">ketvirtas lapas</a>
-                <div class="slide-buttoms text-center">
-                    <button class="left-buttom" onclick="plusDivs(-1)">&#10094;</button>
-                    <button class="right-buttom" onclick="plusDivs(1)">&#10095;</button>
-                </div>
-
             </div>
-                                        <!----------JS-------->
-            <script>
-                var slideIndex = 1;
-                showDivs(slideIndex);
-                function plusDivs(n) {
-                    showDivs(slideIndex += n);
-                }
-                function showDivs(n) {
-                    var i;
-                    var x = document.getElementsByClassName("mySlides");
-                    if (n > x.length) {slideIndex = 1}    
-                    if (n < 1) {slideIndex = x.length}
-                    for (i = 0; i < x.length; i++) {
-                        x[i].style.display = "none";  
-                    }
-                    x[slideIndex-1].style.display = "block";  
-                }
-            </script>
-                                        <!----------JS End-------->
-
+        </div>
+        <div class="slideshow">
+            <div class="w3-content w3-display-container">
+                <a class="mySlides pirmas-slide"></a>
+                <a class="mySlides antras-slide"></a>
+                <a class="mySlides trecias-slide"></a>
+                <a class="mySlides ketvirtas-slide"></a>
+                <button class="left-buttom" onclick="plusDivs(-1)">&#10094;</button>
+                <button class="right-buttom" onclick="plusDivs(1)">&#10095;</button>
+                <div class="banner-line">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-4"></div>
+                            <div class="col-4"></div>
+                            <div class="col-4"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="slide-buttoms text-center"></div>
         </div>
     </div>
+
+
+
 
 
         
