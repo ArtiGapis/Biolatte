af0ea0e8f80629c67b36d52bca6e40db182b815c
Arturas <arturas.danilevicius@gmail.com>
2018-10-14 22:25:36 +0300

    index update

--- index.php
+++ index.php
@@ -1,2 +1,90 @@
     <?php include 'header.php';  ?>
+    <div class="pagrindas">
+        <div class="pirma-pakopa">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <div class="lizdas text-center">
+                            <img src="image/bottle1.jpg">
+                            <h4>biolatte original 110 kapslit</h4>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <h6>33.60</h6>
+                                        <h5>26.80</h5>
+                                    </div>
+                                    <div class="col-8">
+                                        <a href="#" class="clikas">lisa ostukorvi</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="lizdas text-center">
+                            <img src="image/bottle1.jpg">
+                            <h4>biolatte original 110 kapslit</h4>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <h6>33.60</h6>
+                                        <h5>26.80</h5>
+                                    </div>
+                                    <div class="col-8">
+                                        <a href="#" class="clikas">lisa ostukorvi</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="lizdas text-center">
+                            <img src="image/bottle1.jpg">
+                            <h4>biolatte original 110 kapslit</h4>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <h6>33.60</h6>
+                                        <h5>26.80</h5>
+                                    </div>
+                                    <div class="col-8">
+                                        <a href="#" class="clikas">lisa ostukorvi</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="lizdas text-center">
+                            <img src="image/bottle1.jpg">
+                            <h4>biolatte original 110 kapslit</h4>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <h6>33.60</h6>
+                                        <h5>26.80</h5>
+                                    </div>
+                                    <div class="col-8">
+                                        <a href="#" class="clikas">lisa ostukorvi</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="antra-pakopa">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <a href="#" class="mini-lizdas">
+                            <img src="image/users0.jpg">
+                            <h3>probiotikumid</h3>
+                        </a>
+                        <a class="mini-lizdas"></a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="trecia-pakopa"></div>
+        <div class="ketvirta-pakopa"></div>
+        <div class="penkta-pakopa"></div>
+    </div>
     <?php include 'footer.php';?>
