be89ec3596b06b34fb1be342604bd35b41b7f0f6
Arturas <arturas.danilevicius@gmail.com>
2018-10-18 09:56:14 +0300

    finish INDEX

--- index.php
+++ index.php
@@ -130,21 +130,53 @@
                         <div class="transport text-center">
                             <img src="image/buda.png">
                             <h1>soodinetransport</h1>
+                            <p>SmartPost</p>
                         </div>
                     </div>
                     <div class="col-4">
                         <div class="kareivis text-center">
                             <img src="image/kareiviskas.png">
+                            <h1>hinnagarantii</h1>
+                            <p>Garantereme</p>
                         </div>
                     </div>
                     <div class="col-4">
                         <div class="krepselis text-center">
                             <img src="image/krepselis.png">
+                            <h1>tagastusoigas</h1>
+                            <p>tagas</p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="penkta-pakopa"></div>
+        <div class="penkta-pakopa">
+            <div class="container">
+                <div class="row">
+                    <div class="col-7">
+                        <div class="tekstas">
+                            <h1>Biolatest</h1>
+                            <p>The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the layout. A practice not without controversy, laying out pages with meaningless filler text can be very useful when the focus is meant to be on design, not content. The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum.
+                            <br>
+                                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                        </div>
+                    </div>
+                    <div class="col-5">
+                        <div class="fb">
+                            <h1>Biolatte facebookis</h1>
+                            <div id="fb-root"></div>
+                            <script>(function(d, s, id) {
+                                    var js, fjs = d.getElementsByTagName(s)[0];
+                                    if (d.getElementById(id)) return;
+                                    js = d.createElement(s); js.id = id;
+                                    js.src = 'https://connect.facebook.net/lt_LT/sdk.js#xfbml=1&version=v3.1';
+                                    fjs.parentNode.insertBefore(js, fjs);
+                                }(document, 'script', 'facebook-jssdk'));</script>
+                            <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=340&height=400&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="400" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <?php include 'footer.php';?>
